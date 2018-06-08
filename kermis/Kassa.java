39b0f9a1a79c4945f9c642c97c1ec895e0d0479b
MartineSr <martineschroor@gmail.com>
2018-06-08 13:47:49 +0200

    kermis stap 2.1 werkend

--- kermis/Kassa.java
+++ kermis/Kassa.java
@@ -1,31 +1,65 @@
 package kermis;
 
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Kassa {  	
 	double omzet;
 	double prijs;
+	boolean doorgaan;
+	
 
 	ArrayList<Object> Attracties = new ArrayList<>();
+	ArrayList<Double> AttractiePrijs = new ArrayList<>();
 	
 	void Lijst() {
-	Attracties.add(new Spookhuis().naam = "Spookhuis");
+	Attracties.add(new Spookhuis().naam = "Tomb of Horrors");
 	Attracties.add(new Botsauto().naam = "Botsauto");
-	Attracties.add(new Ladderklimmen().naam = "Ladderklimmen");
+	Attracties.add(new Ladderklimmen().naam = "Ladders without Snakes");
 	Attracties.add(new Hawaii().naam = "Hawaii");
 	Attracties.add(new Spin().naam = "Spin");
 	Attracties.add(new Spiegelpaleis().naam = "Spiegelpaleis");
-	System.out.println(Attracties.toString());
+	System.out.println("Op de kermis staan de volgende attracties: " +Attracties.toString());
 	}
 	
+	void Prijslijst() {
+		AttractiePrijs.add(3.20);
+		AttractiePrijs.add(2.50);
+		AttractiePrijs.add(5.00);
+		AttractiePrijs.add(2.90);
+		AttractiePrijs.add(2.25);
+		AttractiePrijs.add(2.75);
+		System.out.println("Deze attracties kosten: " +AttractiePrijs.toString());
+	}
+		
+	Scanner sc = new Scanner(System.in);
+	
 	public void vragen() {
-	System.out.println("In welke attractie wil je?");
+		System.out.println("Wil je iets doen op de kermis?");
+		doorgaan = sc.nextBoolean();
+		if(doorgaan == true ) {
+			System.out.println("In welke attractie wil je?");
+		}
+		else {
+			System.out.println("Tot ziens!");
+			System.exit(0);
+		}
 	}	
 		
 	public void aanzettenAttractie(int userInput) {	
 		System.out.println("De kassapersoon drukt op de knop!");		
 		Attracties.get(userInput);
 	}
+	
+	public void Omzet(int userInput) {
+		AttractiePrijs.get(userInput);
+		System.out.println("Deze attractie heeft " +AttractiePrijs.get(userInput) +" opgeleverd.");
+		omzet += AttractiePrijs.get(userInput);
+		System.out.println("De omzet op de kermis is: "+omzet);
+	}
+	
+
+	
 	
 	
 	
--- kermis/Kermisje.java
+++ kermis/Kermisje.java
@@ -8,28 +8,26 @@ public class Kermisje {
 
 		Attractie attractie = new Attractie();		
 		Kassa k = new Kassa();
-		
-		/*ArrayList<Object> Attracties = new ArrayList<>();
-		
-		Attracties.add(new Spookhuis().naam = "Spookhuis");
-		Attracties.add(new Botsauto().naam = "Botsauto");
-		Attracties.add(new Ladderklimmen().naam = "Ladderklimmen");
-		Attracties.add(new Hawaii().naam = "Hawaii");
-		Attracties.add(new Spin().naam = "Spin");
-		Attracties.add(new Spiegelpaleis().naam = "Spiegelpaleis");
-		
-		System.out.println(Attracties.toString());*/
 
-		// Het daadwerkelijk draaien van de kermis.
-		k.Lijst();		
-		k.vragen();			
+		k.Lijst();	
+		k.Prijslijst();
 		
 		Scanner scan = new Scanner(System.in);
-		int userInput = scan.nextInt();
+		do {
+		k.vragen();		
+		if(k.doorgaan == true) {
+			int userInput = scan.nextInt();
+				
+			k.aanzettenAttractie(userInput);
+			attractie.draaien(k.Attracties.get(userInput));
 		
-		k.aanzettenAttractie(userInput);
-		
-		attractie.draaien(k.Attracties.get(userInput));
+			k.Omzet(userInput);
+		}
+		else {
+			System.out.println("Tot ziens!");
+			System.exit(0);
+		}
+		}while(k.doorgaan == true);
 
 }
 }
